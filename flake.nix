91090a2c096832bfea9ebb08909e715a2a858aed
Brandon Buckley <brandonmb0136@gmail.com>
2024-08-22 15:33:28 +0000

    initial commit

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, flake-utils, ... }: flake-utils.lib.eachDefaultSystem (system: let
+    pkgs = nixpkgs.legacyPackages.${system};
+    tools = [
+      pkgs.gcc8 # Version used in AU Linux Lab
+    ];
+    mkCppApp = { name, src, main }: pkgs.stdenv.mkDerivation {
+      inherit name system src;
+      nativeBuildInputs = tools;
+      buildPhase = "g++ -Wall ${main}";
+      installPhase = ''
+      mkdir-p $out/bin
+      cp a.out $out/bin/${name}
+      chmod +x $out/bin/${name}
+      '';
+    };
+  in {
+    devShells.default = pkgs.mkShell {
+      nativeBuildInputs = tools;
+      shellHook = ''
+      export PS1="\n[\e[1;32m\[nix develop:\w]\$\e[0m "
+      '';
+    };
+  });
+}
