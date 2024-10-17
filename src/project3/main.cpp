5a18a55baa3bb4881151072bc6e6ff9345790e20
Brandon Buckley <brandonmb0136@gmail.com>
2024-10-17 16:07:46 -0500

    Get number of files to read

--- src/project3/main.cpp
+++ src/project3/main.cpp
@@ -1,3 +1,21 @@
+#include <iostream>
+using namespace std;
+
 int main() {
+  cout << "*** Welcome to Brandon's Data Analyzer ***" << endl;
+
+  int files;
+  while (true) {
+    cout << "Enter the number of files to read: ";
+    cin >> files;
+    if (cin.fail()) {
+      cin.clear();
+      cin.ignore(99999, '\n');
+      cout << "Invalid number" << endl;
+    } else {
+      break;
+    }
+  }
+
   return 0;
 }
