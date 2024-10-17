a951d823895ba587a4ad5c080f7e86cca6a640a8
Brandon Buckley <brandonmb0136@gmail.com>
2024-10-17 17:37:16 -0500

    Added basic file parsing

--- src/project3/main.cpp
+++ src/project3/main.cpp
@@ -1,4 +1,6 @@
+#include <fstream>
 #include <iostream>
+#include <sstream>
 #include "list.h"
 
 using namespace std;
@@ -6,14 +8,64 @@ using namespace std;
 template class SortedList<float>;
 
 int getFileCount();
+bool tryParseFile(SortedList<float>& data, ifstream& file);
 
 int main() {
   cout << "*** Welcome to Brandon's Data Analyzer ***" << endl;
 
   int files = getFileCount();
 
+  SortedList<float> data;
+  for (int i = 0; i < files; i++) {
+    string path;
+
+    cout << "Enter the file name for file " << (i + 1) << ": ";
+    cin >> path;
+    if (path == "quit") {
+      break;
+    }
+
+    ifstream file(path);
+    if (!file.is_open()) {
+      cout << "File not found. Please try again" << endl;
+      i--;
+      continue;
+    }
+
+    SortedList<float> dataFromFile;
+    if (!tryParseFile(dataFromFile, file)) {
+      cout << "Not an input file. Illegal content/structure detected. Please try again" << endl;
+      i--;
+      continue;
+    }
+  }
+
+  for (int i = 0; i < data.size(); i++) {
+    cout << data.get(i) << endl;
+  }
+
   cout << "*** Goodbye ***" << endl;
   return 0;
+}
+
+bool tryParseFile(SortedList<float>& data, ifstream& file) {
+  while (!file.eof()) {
+    string line;
+    getline(file, line);
+    if (line == "") {
+      break;
+    }
+    stringstream lineStream(line);
+
+    float value;
+    lineStream >> value;
+    if (lineStream.fail()) {
+      return false;
+    }
+
+    data.add(value);
+  }
+  return true;
 }
 
 int getFileCount() {
