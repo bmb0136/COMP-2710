b3a1b4012d6d3510c83713ec3b10df1db5dcdf92
Brandon Buckley <brandonmb0136@gmail.com>
2024-10-21 13:22:21 -0500

    Replace linear scan with bin search

--- src/project3/list.h
+++ src/project3/list.h
@@ -9,13 +9,20 @@ private:
 public:
   int size() { return data.size(); }
   void add(T value) {
-    for (int i = 0; i < data.size(); i++) {
-      if (value < data[i]) {
-        data.insert(data.begin() + i, value);
-        return;
+    int left = 0, right = data.size() - 1;
+
+    while (left <= right) {
+      int mid = (left + right) / 2;
+      T midVal = data[mid];
+
+      if (midVal < value) {
+        left = mid + 1;
+      } else {
+        right = mid - 1;
       }
     }
-    data.push_back(value);
+
+    data.insert(data.begin() + left, value);
   }
   T get(int i) {
     return data[i];
