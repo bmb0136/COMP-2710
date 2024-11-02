bcf3b9e4f173c79710c4be81d39e8f96f2275929
Brandon Buckley <brandonmb0136@gmail.com>
2024-11-01 19:17:17 -0500

    Impl DataPoint

--- src/project3/data.h
+++ src/project3/data.h
@@ -1,21 +1,87 @@
 #ifndef DATA_H
 #define DATA_H
 
+#include <sstream>
+#include <iomanip>
 #include <string>
-using std::string;
+using namespace std;
+
+static bool read2DigitPaddedNum(stringstream& ss, int& value, int min, int max) {
+  char tens, ones;
+
+  ss >> tens;
+  if (ss.fail() || tens < '0' || tens > '9') {
+    return false;
+  }
+
+  ss >> ones;
+  if (ss.fail() || ones < '0' || ones > '9') {
+    return false;
+  }
+
+  value = (10 * (tens - '0')) + (ones - '0');
+
+  return value >= min && value <= max;
+}
 
 struct DataPoint {
 public:
   float value;
   int hour, minute, second;
   string toString() {
-    return "TODO";
+    stringstream ss;
+
+    ss << value << '\t';
+    ss << setw(2) << setfill('0');
+    ss << hour << '.' << minute << '.' << second;
+
+    return ss.str();
   }
   static bool fromString(string str, DataPoint& dp) {
-    return false;
-  }
-  bool operator<(DataPoint other) {
-    return false;
+    stringstream ss(str);
+
+    // Read value
+    ss >> dp.value;
+    if (ss.fail()) {
+      return false;
+    }
+
+    // Read \t
+    if (ss.get() != '\t') {
+      return false;
+    }
+
+    // Read hour
+    if (!read2DigitPaddedNum(ss, dp.hour, 0, 24)) {
+      return false;
+    }
+   
+    // Read .
+    if (ss.get() != '.') {
+      return false;
+    }
+
+    // Read minute
+    if (!read2DigitPaddedNum(ss, dp.minute, 0, 60)) {
+      return false;
+    }
+   
+    // Read .
+    if (ss.get() != '.') {
+      return false;
+    }
+    
+    // Read second
+    if (!read2DigitPaddedNum(ss, dp.second, 0, 60)) {
+      return false;
+    }
+
+    // Check for \n or eof
+    if (ss.get() != '\n' && !ss.eof()) {
+      return false;
+    }
+
+    return true;
   }
 };
 
